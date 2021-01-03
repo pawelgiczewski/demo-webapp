0a180daf80d1c27d700252fec6d25cb9ac2f9a67
Pawel Giczewski <>
2021-01-03 20:18:30 +0100

    add primevue and display message in toast

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,9 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <button v-on:click="setRandomCatFact">Ask server for random cat fact</button>
-    <p>
-      {{ randomCatFact }}
-    </p>
+
+    <Message severity="success">{{ randomCatFact }}</Message>
+
     <h3>Credits</h3>
     <div>Check out <a href="https://github.com/pawelgiczewski/demo-webapp" target="_blank">the repository on GitHub</a>.</div>
     <div>Icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Message from 'primevue/message';
+import Dialog from 'primevue/dialog';
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue, {ripple: true});
+app.component('Message', Message);
+app.component('Dialog', Dialog);
+app.mount('#app');
